caa75cca3569b42e3d86629d2b45a431cafd5ffd
Masataka Hirano <alpaca0984@gmail.com>
2023-12-31 03:46:32 +0100

    fix: EDITOR path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,11 +30,12 @@ alias rm="rm -i"
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 if type -q nvim
-  set -x EDITOR "/usr/local/bin/nvim"
+  set -x EDITOR (which nvim)
   alias vim="nvim"
 end
 
-# @see https://powerline.readthedocs.io/en/latest/installation/osx.html#python-package
+
+# See https://powerline.readthedocs.io/en/latest/installation/osx.html#python-package
 # This must be below pyenv init because `pip` command depends on it
 set -x POWERLINE_REPOSITORY_ROOT (pip show powerline-status | grep Location | cut -d ' ' -f 2)
 
