1673077d6c8cd866fba8ea8555a81d9cca434536
Masataka Hirano <alpaca0984@gmail.com>
2024-07-13 19:12:53 +0200

    feat(devcontainer): setup script

--- platform/devcontainer/setup.sh
+++ platform/devcontainer/setup.sh
@@ -61,8 +61,6 @@ if [[ ! -d "${ZSH_PLUGINS}/zsh-syntax-highlighting" ]]; then
     "${ZSH_PLUGINS}/zsh-syntax-highlighting"
 fi
 
-source $HOME/.zshrc
-
 # vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
